4dbb2854e598f5fe8740ecb823b8b2ecf6ae41b8
Martin Knudsen <martinknud@gmail.com>
2021-09-23 13:06:43 +0200

    upgraded gradle files and kotlin

--- app/src/main/java/org/pondar/layoutbottomscreenkotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/layoutbottomscreenkotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package org.pondar.layoutbottomscreenkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.pondar.layoutbottomscreenkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
